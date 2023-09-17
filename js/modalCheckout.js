db817d57859eaa7ebb79d339f3ece5b1c484da79
stiansame <stianrostad@gmail.com>
2023-09-17 12:16:05 +0200

    modal

--- js/modalCheckout.js
+++ js/modalCheckout.js
@@ -1,23 +1,12 @@
-const buyButton = document.getElementById("buytBtn");
-const closeButton = document.getElementById("closeBtn");
-const checkoutDialog = document.getElementById("checkoutDialog");
+const checkoutModal = document.getElementById("checkoutDialog");
+const openCheckoutModal = document.getElementById("purchase");
 
-function openCheck(dialog) {
-  if (dialog.open) {
-    console.log("Dialog open");
-  } else {
-    console.log("Dialog closed");
-  }
-}
+const closeCheckoutModal = document.getElementById("closeBtn");
 
-// Update button opens a modal dialog
-buyButton.addEventListener("click", () => {
-  checkoutDialog.showModal();
-  openCheck(dialog);
+openCheckoutModal.addEventListener("click", () => {
+  checkoutModal.showModal();
 });
 
-// Form cancel button closes the dialog box
-closeButton.addEventListener("click", () => {
-  checkoutDialog.close("cancel_login");
-  openCheck(dialog);
+closeCheckoutModal.addEventListener("click", () => {
+  checkoutModal.close();
 });
